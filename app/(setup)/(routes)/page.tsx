90e8fcd615c91e84224cd5a02f8fe55191c44e52
Ouambo Patrick <ouambotakouam@gmail.com>
2023-10-03 19:26:38 +0200

    initial profile code added

--- app/(setup)/(routes)/page.tsx
+++ app/(setup)/(routes)/page.tsx
@@ -1,10 +1,24 @@
-import { ModeToggle } from "@components/mode-toggle";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  return (
-    <div>
-      <p>Protected route</p>
-      <ModeToggle />
-    </div>
-  );
+import { db } from "@lib/db";
+import initialprofile from "@lib/intial-profile";
+
+export default async function Home() {
+  const profile = await initialprofile();
+
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/server/${server.id}`);
+  }
+
+  return <div>Initial modal</div>;
 }
