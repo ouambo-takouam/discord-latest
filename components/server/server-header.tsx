755b7b3d93fd4e422030108d8cc8744250b6ca90
Ouambo Patrick <ouambotakouam@gmail.com>
2023-10-04 16:27:44 +0200

    modal hook rebuilt

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -2,8 +2,10 @@
 
 import {
   ChevronDown,
+  LogOut,
   PlusCircle,
   Settings,
+  Trash,
   UserPlus,
   Users,
 } from "lucide-react";
@@ -15,6 +17,7 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@components/ui/dropdown-menu";
 
@@ -61,6 +64,22 @@ export default function ServerHeader({ server, role }: ServerHeaderProps) {
           <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
             Create channel
             <PlusCircle className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+
+        {isModerator && <DropdownMenuSeparator />}
+
+        {isAdmin && (
+          <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+            Delete Server
+            <Trash className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+
+        {!isAdmin && (
+          <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+            Leave Server
+            <LogOut className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
         )}
       </DropdownMenuContent>
